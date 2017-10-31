e56509088878233a7732c8a7f1192301d4033aa2
Sylvain Baronnet <sylvain@textmaster.com>
2017-10-31 16:23:45 +0200

    test

--- src/Box.js
+++ src/Box.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { space, width, removeProps, propTypes } from 'styled-system'
+import { space, width } from 'styled-system'
 import styled from './styled'
 
-const CleanDiv = removeProps('div');
-
-CleanDiv.propTypes = {
-  ...propTypes.width,
-  ...propTypes.wrap,
-}
+// hoc to remove unwanted width attribute
+const hoc = Comp => ({width, wrap, ...props}) => <Comp {...props} w={width} />
 
 export const flex = ({flex}) => flex ? {flex} : null
 
-const Box = styled(CleanDiv,
+const Box = hoc(styled('div',
   {boxSizing: 'border-box'},
   width,
   space,
   flex
-)
-
-
-
+))
 
 export default Box
